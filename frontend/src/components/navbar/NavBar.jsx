7fd6ce21ab5c9de12c1f7a5650640b0ec0983643
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-04-27 02:43:08 -0300

    atualizado o componente NavBar com nova imagem de perfil e melhorias no layout

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import Historico from "../../assets/Header/Botão - Histórico.png";
 import Home from "../../assets/Header/botão - Home.png";
 import Pesquisa from "../../assets/Header/Botão - Pesquisa.png";
@@ -6,44 +7,89 @@ import Sair from "../../assets/Header/Botão - Sair.png";
 import Perfil from "../../assets/Header/image.png";
 import Fechar from "../../assets/Header/imagem-fechar-header.png";
 import styles from "./NavBar.module.scss";
-import { useNavigate } from "react-router-dom";
+import fotoPerfil from "../../assets/Tela_Login/fotoPerfilPatrick.jpg"
 
 function NavBar() {
   const navigate = useNavigate();
 
   return (
-    <nav className={styles.navBarEstilo}>
-      <button className={styles.buttonNavBar}>
+    <nav className={styles.navBar}>
+
+      {/* ABRIR E FECHAR */}
+      <button className={styles.buttonAbrirFechar}>
         <img src={Fechar} alt="Botão abrir e fechar" />
       </button>
 
+      {/* FOTO PERFIL */}
+      <div className={styles.divFotoPerfil}>
+      <img src={fotoPerfil} alt="Foto do perfil" />
+        <h3>Patrick leonardo</h3>
+        <p>Programador Senior</p>
+
+      </div>
+      
       <section>
-        <img src={Perfil} alt="Foto do perfil" />
-
         <div>
-          <button className={styles.buttonNavBar}
-            onClick={() => {
-              navigate("/reembolsos");
-            }}
-          >
-            <img src={Home} alt="Botão Home" />
-          </button>
+          {/* INICIO */}
+          <div className={styles.divButton}>
+            <button
+              className={styles.buttonNavBar}
+              onClick={() => {
+                navigate("/reembolsos");
+              }}
+            >
+              <img src={Home} alt="Botão Home" />
+            </button>
+            <p>Início</p>
+          </div>
+          {/* REEMBOLSOS */}
+          <div className={styles.divButton}>
+            <button
+              onClick={() => {
+                navigate("/solicitacao");
+              }}
+              className={styles.buttonNavBar}
+            >
+              <img src={Reembolso} alt="Botão Reembolso" />
+            </button>
+            <p>Reembolsos</p>
+          </div>
 
-          <button onClick={()=>{navigate("/solicitacao")}}  className={styles.buttonNavBar}>
-            <img src={Reembolso} alt="Botão Reembolso" />
-          </button>
+          {/* ANALISES */}
+          <div className={styles.divButton}>
+            <button
+              onClick={() => {
+                navigate("/reembolsos");
+              }}
+              className={styles.buttonNavBar}
+            >
+              <img src={Pesquisa} alt="Botão Pesquisa" />
+            </button>
+            <p>Análises</p>
+          </div>
 
-          <button onClick={()=>{navigate("/reembolsos")}}  className={styles.buttonNavBar}>
-            <img src={Pesquisa} alt="Botão Pesquisa" />
-          </button>
-
-          <button onClick={()=>{navigate("/solicitacao")}}  className={styles.buttonNavBar}>
-            <img src={Historico} alt="Botão histórico" />
-          </button>
+          {/* HISTORICO */}
+          <div className={styles.divButton}>
+            <button
+              onClick={() => {
+                navigate("/solicitacao");
+              }}
+              className={styles.buttonNavBar}
+            >
+              <img src={Historico} alt="Botão histórico" />
+            </button>
+            <p>Histórico</p>
+          </div>
         </div>
       </section>
 
-      <button className={styles.buttonSair}  onClick={()=>{navigate("/")}} >
+      {/* SAIR */}
+      <button
+        className={styles.buttonSair}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         <img src={Sair} alt="Botão sair" />
       </button>
     </nav>
