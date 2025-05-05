15fc72f21f1fabb3e8954f4fa44d110a10531cd8
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-05-05 01:17:49 -0300

    Refatora modelo de reembolso para corrigir erros de digitação, remover valores padrão desnecessários e adicionar método to_dict para conversão em dicionário.

--- backend/src/model/reembolso_model.py
+++ backend/src/model/reembolso_model.py
@@ -1,6 +1,6 @@
 from src.model import db
 from sqlalchemy.schema import Column, ForeignKey
-from sqlalchemy.types import Integer, String, DECIMAL, Date
+from sqlalchemy.types import Integer, DECIMAL, String, Date
 from sqlalchemy import func
 
 
@@ -13,24 +13,19 @@ class Reembolso(db.Model):
     empresa = Column(String(50), nullable=False)
     numero_prestacao = Column(Integer, nullable=False)
     descricao = Column(String(255))
-    data = Column(Date, nullable=False, server_default=func.current_date())
+    data = Column(Date, nullable=False)
     tipo_reembolso = Column(String(35), nullable=False)
     centro_custo = Column(String(50), nullable=False)
     ordem_interna = Column(String(50))
     divisao = Column(String(50))
     pep = Column(String(50))
     moeda = Column(String(25), nullable=False)
-    # Valor para contas
-    distacia_km = Column(DECIMAL(10, 2), nullable=False)
-    valo_km = Column(DECIMAL(10, 2), nullable=False)
+    distancia_km = Column(DECIMAL(10, 2))
+    valor_km = Column(DECIMAL(10, 2))
     valor_faturado = Column(DECIMAL(10, 2), nullable=False)
-    despesas = Column(
-        DECIMAL(10, 2)
-    )  # valor faturado - o valor da despesa para o calculo
-    id_colaborador = Column(
-        ForeignKey(colum="colaborador.colaborador.id"), nullable=False
-    )
-    status = Column(String(20), nullable=False, default="Pendente")
+    despesas = Column(DECIMAL(10, 2))
+    id_colaborador = Column(Integer, ForeignKey("colaborador.id"), nullable=False)
+    status = Column(String(20), nullable=False)
 
     # Método construtor para facilitar a criação de objetos Colaborador
     def __init__(
@@ -46,12 +41,12 @@ class Reembolso(db.Model):
         divisao,
         pep,
         moeda,
-        distacia_km,
-        valo_km,
+        distancia_km,
+        valor_km,
         valor_faturado,
         despesas,
         id_colaborador,
-        status,
+        status = "Em análise",
     ):
         self.colaborador = colaborador
         self.empresa = empresa
@@ -64,9 +59,31 @@ class Reembolso(db.Model):
         self.divisao = divisao
         self.pep = pep
         self.moeda = moeda
-        self.distacia_km = distacia_km
-        self.valo_km = valo_km
+        self.distancia_km = distancia_km
+        self.valo_km = valor_km
         self.valor_faturado = valor_faturado
         self.despesas = despesas
         self.id_colaborador = id_colaborador
         self.status = status
+
+    def to_dict(self) -> dict:
+        return {
+            "colaborador": self.colaborador,
+            "empresa": self.empresa,
+            "numero_prestacao": self.numero_prestacao,
+            "descricao": self.descricao,
+            "data": self.data,
+            "tipo_reembolso": self.tipo_reembolso,
+            "centro_custo": self.centro_custo,
+            "ordem_interna": self.ordem_interna,
+            "divisao": self.divisao,
+            "pep": self.pep,
+            "moeda": self.moeda,
+            "distancia_km": self.distancia_km,
+            "valor_km": self.valor_km,
+            "valor_faturado": self.valor_faturado,
+            "despesas": self.despesas,
+            "id_colaborador": self.id_colaborador,
+            "status": self.status
+        }
+
