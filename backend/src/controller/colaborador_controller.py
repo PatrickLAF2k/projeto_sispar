c3da3ac5f7bc78832c25491146fdf00d8f5c0ff8
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-05-05 01:16:44 -0300

    Refatora cadastro de colaborador para incluir validações e mensagens de erro apropriadas

--- backend/src/controller/colaborador_controller.py
+++ backend/src/controller/colaborador_controller.py
@@ -1,76 +1,84 @@
 from flask import Blueprint, request, jsonify
 from src.model.colaborador_model import Colaborador
 from src.model import db
+from flasgger import swag_from
+from src.security.security import hash_senha, verificar_senha
 
 bp_colaborador = Blueprint("colaborador", __name__, url_prefix="/colaborador")
 
 
 @bp_colaborador.route("/cadastrar", methods=["POST"])
+@swag_from("../docs/colaborador/cadastrar_colaborador.yml")
 def cadastrar_novo_colaborador():
-    # Pega os dados enviados no corpo da requisição em formato JSON
-    dados_requisicao = request.get_json()
-    # O mais proximo de uma desestruturação em Python
-    nome, email, senha, cargo, salario = (
-        dados_requisicao.get(key)
-        for key in ["nome", "email", "senha", "cargo", "salario"]
-    )
-
-    # Verifica se todos os campos obrigatórios foram preenchidos
-    if not nome or not email or not senha or not cargo or not salario:
-        return jsonify({"mensagem": "Todos os campos são obrigatorios!"}), 400
-
-    if not salario.isdigit():
-        return jsonify({"mensagem": "Salário deve conter apenas números!"}), 400
+    nome = request.json.get("nome")
+    email = request.json.get("email")
+    senha = request.json.get("senha")
+    cargo = request.json.get("cargo")
+    salario = request.json.get("salario")
+    
 
     try:
-        salario = float(salario)
-    except ValueError:
-        return jsonify({"mensagem": "Salário só deve conter números!"}), 400
 
-    # Verifica se o colaborador já existe no banco de dados
-    colaborador_existente = db.session.execute(
-        db.select(Colaborador).where(Colaborador.email == email)
-    ).scalar()
+        if not nome or not email or not senha or not cargo or not salario:
+            return jsonify({"mensagem": "Todos os campos são obrigatorios!"}), 400
 
-    if colaborador_existente:
-        return jsonify({"mensagem": "Email já cadastrado!"}), 409
 
-    # Cria uma nova instância de Colaborador com os dados recebidos
-    novo_colaborador = Colaborador(
-        nome=dados_requisicao["nome"],
-        email=dados_requisicao["email"],
-        senha=dados_requisicao["senha"],
-        cargo=dados_requisicao["cargo"],
-        salario=dados_requisicao["salario"],
-    )
+        colaborador_existente = db.session.execute(
+            db.select(Colaborador).where(Colaborador.email == email)
+        ).scalar()
 
-    # Adiciona o novo colaborador à sessão do banco de dados
-    db.session.add(novo_colaborador)
+        if colaborador_existente:
+            return jsonify({"mensagem": "Email já cadastrado!"}), 409
 
-    # Salva (commit) as mudanças no banco de dados
-    db.session.commit()
+        novo_colaborador = Colaborador(
+            nome=nome,
+            email=email,
+            senha=hash_senha(senha),
+            cargo=cargo,
+            salario=salario,
+        )
 
-    # Retorna uma mensagem de sucesso e o status HTTP 201 (Created)
-    return jsonify({"mensagem": f"Colaborador {nome} cadastrado com sucesso!"}), 201
+        # Adiciona o novo colaborador à sessão do banco de dados
+        db.session.add(novo_colaborador)
+
+        # Salva (commit) as mudanças no banco de dados
+        db.session.commit()
+
+        # Retorna uma mensagem de sucesso e o status HTTP 201 (Created)
+        return jsonify({"mensagem": f"Colaborador {nome} cadastrado com sucesso!"}), 201
+
+    except Exception as e:
+        # Captura qualquer exceção e retorna um erro genérico com a mensagem
+        return jsonify({"erro": f"Ocorreu um erro inesperado: {str(e)}"}), 500
 
 
 @bp_colaborador.route("/login", methods=["POST"])
 def login():
-
     email = request.json.get("email")
     senha = request.json.get("senha")
 
-    if not email or not senha:
-        return jsonify({"mensagem": "Email e senha são obrigatórios!"}), 400
+    try:
 
-    colaborador = db.session.execute(
-        db.select(Colaborador).where(Colaborador.email == email)
-    ).scalar()
+        if not email or not senha:
+            return jsonify({"mensagem": "Email e senha são obrigatórios!"}), 400
 
-    if not colaborador:
-        return jsonify({"mensagem": "Colaborador não encontrado!"}), 404
+        colaborador = db.session.execute(
+            db.select(Colaborador).where(Colaborador.email == email)
+        ).scalar()
 
-    if not colaborador.senha == senha and colaborador.email == email:
-        return jsonify({"mensagem": "Email ou senha Incorretos!"}), 401
+        if not colaborador:
+            return jsonify({"mensagem": "Colaborador não encontrado!"}), 404
 
-    return jsonify({"mensagem": "Login realizado com sucesso!"}), 200
+        if  verificar_senha(senha, colaborador.senha):
+            return jsonify({"mensagem": "Login realizado com sucesso!"}), 200
+        else:
+            return jsonify({"mensagem": "Email ou senha incorretos!"}), 401
+
+    except Exception as e:
+        # Captura qualquer exceção e retorna um erro genérico com a mensagem
+        return jsonify({"erro": f"Ocorreu um erro inesperado: {str(e)}"}), 500
+
+
+@bp_colaborador.route("/logout", methods=["POST"])
+def logout():
+    return jsonify({"mensagem": "Logout realizado com sucesso!"}), 200
