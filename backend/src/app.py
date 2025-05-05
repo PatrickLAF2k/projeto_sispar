c76fda9bb63069f077a8ab81cba7b164fe7ce02d
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-05-05 01:16:23 -0300

    Adicionado o Swagger

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,35 +1,57 @@
-# Importa a classe Flask para criar a aplicação
-from flask import Flask
+from flask import Flask, session
 from flask_cors import CORS
-
-# Importa o blueprint do colaborador, onde ficam as rotas relacionadas
+from dotenv import load_dotenv
 from src.controller.colaborador_controller import bp_colaborador
-
-# Importa o objeto db (instância do SQLAlchemy)
+from src.controller.reembolso_controller import bp_reembolso
 from src.model import db
-
-# Importa as configurações da aplicação, incluindo a URL do banco de dados
 from config import Config
+from flasgger import Swagger
 
-# Função responsável por criar e configurar a aplicação Flask
+import os
+
+Swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "apispec",
+            "route": "/apispec.json/",
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "Swagger_ui": True,
+    "specs_route": "/apidocs/",
+}
+
+
 def create_app():
-    # Cria uma instância da aplicação Flask
+    # Carrega as variáveis de ambiente do arquivo .env
+    load_dotenv()
+
+    # Cria uma instância do Flask
     app = Flask(__name__)
-    CORS(app, origins='*')
 
-    # Registra o blueprint do colaborador, conectando as rotas com a aplicação principal
+    # Configura o CORS para permitir requisições de qualquer origem
+    CORS(app, origins="*")
+
+    # Define a chave secreta a partir da variável de ambiente
+    app.secret_key = os.getenv("SECRET_KEY")
+
+    # Registra os blueprints
     app.register_blueprint(bp_colaborador)
+    app.register_blueprint(bp_reembolso)
 
-    # Carrega as configurações da classe Config (ex: conexão com o banco, variáveis .env)
+    # Carrega as configurações
     app.config.from_object(Config)
-    
-    # Inicializa o SQLAlchemy com a aplicação (faz o link entre Flask e banco)
+
+    # Carrega as configurações
     db.init_app(app)
-    
-    # Cria um contexto da aplicação para que o Flask tenha acesso à estrutura do app
+
+    Swagger(app, config=Swagger_config)
+
+    # Cria as tabelas no banco
     with app.app_context():
-        # Cria as tabelas no banco de dados com base nos modelos definidos
         db.create_all()
-        
-    # Retorna a aplicação configurada
+
     return app
