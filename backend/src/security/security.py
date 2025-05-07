0a07817a16ec24efdb6b75cd62f083078eab41c9
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-05-07 02:23:30 -0300

    adicionado a logica para token JWT

--- backend/src/security/security.py
+++ backend/src/security/security.py
@@ -1,4 +1,14 @@
+from flask import jsonify
 from flask_bcrypt import bcrypt
+import jwt
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
+JWT_SECRET = os.getenv("JWT_SECRET")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
+
 
 def hash_senha(senha):
     salt = bcrypt.gensalt()
@@ -7,3 +17,17 @@ def hash_senha(senha):
 
 def verificar_senha(senha, senha_hash):
     return bcrypt.checkpw(senha.encode("utf-8"), senha_hash.encode("utf-8"))
+
+
+def criar_token(dados):
+    token = jwt.encode(dados, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return token
+
+def decodificar_token(token):
+    try:
+        return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    except jwt.ExpiredSignatureError:
+        return jsonify({"erro": "Token expirado!"}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({"erro": "Token inválido!"}), 401
+
