9f88bd0c3d7acdd81bc010bf102cd66459b61fee
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-05-07 02:22:31 -0300

    logica para pegar o id com token e novas rotas

--- backend/src/controller/reembolso_controller.py
+++ backend/src/controller/reembolso_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from src.model.reembolso_model import Reembolso
 from src.model import db
+from src.security.security import decodificar_token
 
 bp_reembolso = Blueprint("reembolso", __name__, url_prefix="/reembolso")
 
@@ -8,10 +9,12 @@ bp_reembolso = Blueprint("reembolso", __name__, url_prefix="/reembolso")
 @bp_reembolso.route("/solicitar", methods=["POST"])
 def solicitar_reembolso():
     dados_requisicao = request.get_json()
-    print(dados_requisicao)
+    header_token = request.headers.get("Authorization")
+    token = header_token.split("Bearer ")[1]
+    id = decodificar_token(token)
 
     try:
-        # Cria uma lista de instâncias de Reembolso a partir dos dados
+
         reembolsos = [
             Reembolso(
                 colaborador=requisicao["colaborador"],
@@ -29,12 +32,11 @@ def solicitar_reembolso():
                 valor_km=requisicao["valor_km"],
                 valor_faturado=requisicao["valor_faturado"],
                 despesas=requisicao["despesas"],
-                id_colaborador=requisicao["id_colaborador"],
+                id_colaborador=id["id"],
             )
             for requisicao in dados_requisicao
         ]
 
-        # Adiciona todos os reembolsos de uma vez no banco de dados
         db.session.bulk_save_objects(reembolsos)
         db.session.commit()
 
@@ -43,18 +45,35 @@ def solicitar_reembolso():
             201,
         )
     except Exception as e:
-        db.session.rollback()  # Faz rollback em caso de erro
+        db.session.rollback()
         return jsonify({"erro": str(e)}), 400
 
 
 @bp_reembolso.route("/listar", methods=["GET"])
 def listar_reembolsos():
+    header_token = request.headers.get("Authorization")
+    token = header_token.split("Bearer ")[1]
+    id = decodificar_token(token)
+
     try:
-    
-        reembolsos = Reembolso.query.filter_by(id_colaborador= "1").all()
+
+        reembolsos = Reembolso.query.filter_by(id_colaborador=id["id"]).all()
 
         return jsonify([reembolso.to_dict() for reembolso in reembolsos]), 200
 
     except Exception as e:
         return jsonify({"erro": str(e)}), 400
     
+@bp_reembolso.route("/listar/prestacao", methods=["POST"])
+def listar_reembolsos_prestacao():
+    dados_requisicao = request.get_json()
+    
+    try:
+        reembolsos = Reembolso.query.filter_by(numero_prestacao=dados_requisicao["numero_prestacao"]).all()
+
+        return jsonify([reembolso.to_dict() for reembolso in reembolsos]), 200
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 400
+
+
+        
