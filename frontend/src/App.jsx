d485e4ed516ea6a071a06e6ad24ea7c26f82fc33
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-05-07 02:24:10 -0300

    refatorado as rotas para visualizar somente o necessario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Login from "./components/login/Login.jsx";
 import Reembolsos from "./components/reembolsos/Reembolsos.jsx";
 import Solicitacao from "./components/solicitacao/Solicitacao.jsx";
+import NavBar from "../src/components/navbar/NavBar.jsx";
 import "./global.scss";
+
+function AppWrapper() {
+  const location = useLocation();
+ 
+  const mostrarNavBar = location.pathname !== "/";
+
+  return (
+    <>
+      {mostrarNavBar && <NavBar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/reembolsos" element={<Reembolsos />} />
+        <Route path="/solicitacao" element={<Solicitacao />} />
+      </Routes>
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/reembolsos" element={<Reembolsos/>} />
-        <Route path="/solicitacao" element={<Solicitacao/>} />
-      </Routes>
+      <AppWrapper />
     </BrowserRouter>
   );
 }
